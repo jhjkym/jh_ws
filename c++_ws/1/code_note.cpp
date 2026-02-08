6717a1b1efe22de8cee319d9faa46e3b109e473a
jhjkym <2801558652@qq.com>
2026-02-09 02:18:45 +0800

    [feat] C++ First Encounter

---
+++ c++_ws/1/code_note.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// 单行注释
+
+/*
+多行注释
+*/
+int main()
+{
+    cout << "Hello, World!" << endl;
+    /* 用于输出 Hello World 的注释
+
+    cout << "Hello World"; // 输出 Hello World
+
+    */
+
+    system("pause");
+    return 0;
+}
